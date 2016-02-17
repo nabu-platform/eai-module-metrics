a581efaff34b7778986fa146989956eb22460d02
alex <alex@nabu.be>
2016-02-17 09:45:23 +0100

    Added timestamp to response

--- src/main/java/be/nabu/eai/module/metrics/beans/MetricOverview.java
+++ src/main/java/be/nabu/eai/module/metrics/beans/MetricOverview.java
@@ -1,12 +1,16 @@
 package be.nabu.eai.module.metrics.beans;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "overview")
 public class MetricOverview {
+	
+	private long timestamp = new Date().getTime();
 	private List<ArtifactMetrics> metrics = new ArrayList<ArtifactMetrics>();
 
 	public List<ArtifactMetrics> getMetrics() {
@@ -15,4 +19,13 @@ public class MetricOverview {
 	public void setMetrics(List<ArtifactMetrics> metrics) {
 		this.metrics = metrics;
 	}
+	
+	@XmlAttribute
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	
 }
